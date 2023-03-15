35e575da36dcc74d8769dbf40ad5ff886bc0a9fe
LightQv <QVivian23@gmail.com>
2023-03-15 21:09:10 +0100

    Burger Menu Done Juste besoin de corriger le problème de dépacement quand il est actif.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /* Début du Script menu-burger */
 
 const menuBurger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobileNav');
+const mobileMenuBackground = document.querySelector('nav');
 
 menuBurger.addEventListener('click', function () {
     menuBurger.classList.toggle('is-active');
+    mobileMenu.classList.toggle('is-active');
+    mobileMenuBackground.classList.toggle('is-active');
 });
