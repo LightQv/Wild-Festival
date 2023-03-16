fcd5a471e3419b16bed49a775031adff430eafb8
LightQv <QVivian23@gmail.com>
2023-03-16 10:20:06 +0100

    Edited css and js to fix burger menu issues

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
 const menuBurger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobileNav');
 const mobileMenuBackground = document.querySelector('nav');
+const welcomePage = document.querySelector('.welcomePage');
 
 menuBurger.addEventListener('click', function () {
     menuBurger.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
     mobileMenuBackground.classList.toggle('is-active');
+    welcomePage.classList.toggle('is-inactive');
 });
