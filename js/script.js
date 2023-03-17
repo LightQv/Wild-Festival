410765ccc23c1d36817388587d4071fc3e2f15e4
LightQv <QVivian23@gmail.com>
2023-03-17 12:32:47 +0100

    'Fixed HomePage + Navbar'

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ const menuBurger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobileNav');
 const mobileMenuBackground = document.querySelector('nav');
 const welcomePage = document.querySelector('.welcomePage');
+const downArrow = document.querySelector('.down-arrow');
 
 menuBurger.addEventListener('click', function () {
     menuBurger.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
     mobileMenuBackground.classList.toggle('is-active');
     welcomePage.classList.toggle('is-inactive');
+    downArrow.classList.toggle('is-inactive');
 });
 
 /* Fin du Script menu-burger */
