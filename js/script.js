9413e4336fb74738d0657198c553138f1b61ed3b
Yann Richard <yann.richard9@gmail.com>
2023-03-20 17:31:31 +0100

    nabvar removed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// Début du Script Menu-burger
-
-const menuBurger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobileNav');
-const mobileMenuBackground = document.querySelector('nav');
-const pageContainer = document.querySelector('.pageContainer');
-
-menuBurger.addEventListener('click', function () {
-    menuBurger.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-active');
-    mobileMenuBackground.classList.toggle('is-active');
-    pageContainer.classList.toggle('is-inactive');
-});
-
-/* Fin du Script Menu-burger */
-
 // Début du Script Billeterie (flip card)
 
 
@@ -41,5 +25,7 @@ function flipCard(e) {
     break}
 
     card.classList.toggle("flipCard")}
+
+// Fin du script billeterie Flip Card
 
 
