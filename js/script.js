f3c800e8dfe8eed18fa598461c3817ea310b3680
LightQv <QVivian23@gmail.com>
2023-03-21 10:47:44 +0100

    Media querry added

--- js/script.js
+++ js/script.js
@@ -54,36 +54,38 @@ setInterval(findDate, 1000);
 
 /* Fin du Script Countdown */
 
+/* Début du Script Slider */
 
-// Script Slider
-
+//Slider 1
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
 nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
 });
 
 prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
 });
-//slider 2
+
+//Slider 2
 const slidesContainer2 = document.getElementById("slides-container2");
 const slide2 = document.querySelector(".slide2");
 const prevButton2 = document.getElementById("slide-arrow-prev2");
 const nextButton2 = document.getElementById("slide-arrow-next2");
 
 nextButton2.addEventListener("click", () => {
-  const slideWidth = slide2.clientWidth;
-  slidesContainer2.scrollLeft += slideWidth;
+    const slideWidth = slide2.clientWidth;
+    slidesContainer2.scrollLeft += slideWidth;
 });
 
 prevButton2.addEventListener("click", () => {
-  const slideWidth = slide2.clientWidth;
-  slidesContainer2.scrollLeft -= slideWidth;
+    const slideWidth = slide2.clientWidth;
+    slidesContainer2.scrollLeft -= slideWidth;
 });
 
+/* Fin du Script Slider */
