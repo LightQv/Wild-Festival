7b9e13fd2c78d32ff238ebe7c68820a9ccb9f5da
LightQv <QVivian23@gmail.com>
2023-03-21 11:21:43 +0100

    EVERYTHING MERGED

--- js/programmation.js
+++ js/programmation.js
@@ -1,26 +1,22 @@
-
-
-  
 /* Début du Script Menu-burger */
 
 const menuBurger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobileNav');
 const mobileMenuBackground = document.querySelector('nav');
 const pageContainer = document.querySelector('.pageContainer');
+const footerContainer = document.querySelector('.footer');
 
 menuBurger.addEventListener('click', function () {
-    menuBurger.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-active');
-    mobileMenuBackground.classList.toggle('is-active');
-    pageContainer.classList.toggle('is-inactive');
+  menuBurger.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
+  mobileMenuBackground.classList.toggle('is-active');
+  pageContainer.classList.toggle('is-inactive');
+  footerContainer.classList.toggle('is-inactive');
 });
 
 /* Fin du Script Menu-burger */
 
-
-
-
-// Script Slider
+/* Début du Script Slider */
 
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
@@ -36,7 +32,9 @@ prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slidesContainer.scrollLeft -= slideWidth;
 });
+
 //slider 2
+
 const slidesContainer2 = document.getElementById("slides-container2");
 const slide2 = document.querySelector(".slide2");
 const prevButton2 = document.getElementById("slide-arrow-prev2");
@@ -52,3 +50,4 @@ prevButton2.addEventListener("click", () => {
   slidesContainer2.scrollLeft -= slideWidth;
 });
 
+/* Fin du Script Slider */
