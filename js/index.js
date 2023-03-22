c3b328dbe1014082483726233ce240a347a078c2
LightQv <QVivian23@gmail.com>
2023-03-22 10:16:59 +0100

    burger.js + maj html

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/* Début du Script Menu-burger */
-
-const menuBurger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.mobileNav');
-const mobileMenuBackground = document.querySelector('nav');
-const pageContainer = document.querySelector('.pageContainer');
-const footerContainer = document.querySelector('.footer');
-
-menuBurger.addEventListener('click', function () {
-    menuBurger.classList.toggle('is-active');
-    mobileMenu.classList.toggle('is-active');
-    mobileMenuBackground.classList.toggle('is-active');
-    pageContainer.classList.toggle('is-inactive');
-    footerContainer.classList.toggle('is-inactive');
-});
-
-/* Fin du Script Menu-burger */
-
 /* Debut du Script Countdown */
 
 function findDate() {
